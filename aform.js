5bcb0dc192d1d0f5ed705b7c3f9dcc87cd51dbe1
wizardwid <s2335@e-mirim.hs.kr>
2024-10-14 19:47:43 +0900

    css 초반부 수정

--- aform.js
+++ aform.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const dormitoryInput = document.getElementById('room-num');
     const dormiRadioButtons = document.querySelectorAll('input[name="dormi"]');
-  
+    
+    // 기숙사 여부 선택 시 호실 입력란 표시 여부
     dormiRadioButtons.forEach(function(radio) {
         radio.addEventListener('change', function() {
             if (this.value === 'dormitory') {
@@ -10,5 +11,15 @@ document.addEventListener('DOMContentLoaded', function() {
                 dormitoryInput.style.display = 'none';  // 통학 선택 시 호실 입력란 숨기기
             }
         });
+    });
+    
+    // 폼 제출 시 모달을 띄우는 스크립트
+    document.getElementById("applicationForm").addEventListener("submit", function(event) {
+        // 기본 제출 동작 막기
+        event.preventDefault();
+        
+        // 서버로 제출하는 대신 모달 띄우기
+        var myModal = new bootstrap.Modal(document.getElementById('myModal'));
+        myModal.show();
     });
 });
